4c414b8028a986dfaad55b2db1d14e29b12ae7df
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2015-05-01 10:03:10 +0200

    consisntent code style

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
-const Promise = require('es6-promise').Promise
-const expect = require('chai').expect
-const Schema = require('idb-schema')
-const request = require('../lib')
+var Promise = require('es6-promise').Promise
+var expect = require('chai').expect
+var Schema = require('idb-schema')
+var request = require('../lib')
 if (!global.indexedDB) require('indexeddbshim')
-const idb = global.indexedDB
+var idb = global.indexedDB
 
 // force to use a Promise implementation, built on top of microtasks, allowing transaction reuse
 // https://stackoverflow.com/questions/28388129/inconsistent-interplay-between-indexeddb-transactions-and-promises
@@ -12,12 +12,12 @@ request.Promise = Promise
 
 // NOTE:
 // Transaction reuse is not implemeneted right in Safari and WebsqlShim.
-// So we create new transaction for each request to avoid issues.
+// So we create new transaction for each async tick to avoid issues.
 
 describe('idb-request', function() {
-  let db
-  const dbName = 'idb-request1'
-  const schema = new Schema()
+  var db
+  var dbName = 'idb-request'
+  var schema = new Schema()
   .version(1)
     .addStore('books', { key: 'isbn' })
     .addIndex('byTitle', 'title', { unique: true })
@@ -30,61 +30,61 @@ describe('idb-request', function() {
     .addIndex('byPublisher', 'publisher')
     .addIndex('byFrequency', 'frequency')
 
-  beforeEach(() => {
-    const req = idb.open(dbName, schema.version())
+  beforeEach(function() {
+    var req = idb.open(dbName, schema.version())
     req.onupgradeneeded = schema.callback()
-    return request(req).then((origin) => {
+    return request(req).then(function(origin) {
       db = origin
     })
   })
 
-  afterEach(() => {
+  afterEach(function() {
     db.close()
-    return timeout().then(() => {
-      const req = idb.deleteDatabase(db.name)
+    return timeout().then(function() {
+      var req = idb.deleteDatabase(db.name)
       return request(req)
     })
 
     // wrap setTimeout to promise, to handle block errors with mocha
     function timeout() {
-      return new Promise((resolve) => {
-        setTimeout(() => { resolve() }, 50)
+      return new Promise(function(resolve) {
+        setTimeout(function() { resolve() }, 50)
       })
     }
   })
 
-  it('request(db)', () => {
+  it('request(db)', function() {
     expect(db.name).equal(dbName)
     expect(db.version).equal(schema.version())
     expect([].slice.call(db.objectStoreNames)).eql(['books', 'magazines'])
   })
 
-  it('request(req)', () => {
-    let wBook = db.transaction(['books'], 'readwrite').objectStore('books')
+  it('request(req)', function() {
+    var wBook = db.transaction(['books'], 'readwrite').objectStore('books')
     return Promise.all([
       request(wBook.put({ title: 'Quarry Memories', author: 'Fred', isbn: 123456 })),
       request(wBook.put({ title: 'Water Buffaloes', author: 'Fred', isbn: 234567 })),
       request(wBook.put({ title: 'Bedrock Nights', author: 'Barney', isbn: 345678 })),
-    ]).then(() => {
-      const rBooks = db.transaction(['books'], 'readonly').objectStore('books')
-      return request(rBooks.count()).then((count) => {
+    ]).then(function() {
+      var rBooks = db.transaction(['books'], 'readonly').objectStore('books')
+      return request(rBooks.count()).then(function(count) {
         expect(count).equal(3)
       })
     })
   })
 
-  it('request(req, tr)', () => {
-    const tr = db.transaction(['magazines'], 'readwrite')
-    const magazines = tr.objectStore('magazines')
-    const req = magazines.put({ name: 'My magazine' })
-    return request(req, tr).then((id) => {
+  it('request(req, tr)', function() {
+    var tr = db.transaction(['magazines'], 'readwrite')
+    var magazines = tr.objectStore('magazines')
+    var req = magazines.put({ name: 'My magazine' })
+    return request(req, tr).then(function(id) {
       expect(id).equal(1)
     })
   })
 
-  it('request(tr)', () => {
-    const tr = db.transaction(['magazines'], 'readwrite')
-    const magazines = tr.objectStore('magazines')
+  it('request(tr)', function() {
+    var tr = db.transaction(['magazines'], 'readwrite')
+    var magazines = tr.objectStore('magazines')
 
     return Promise.all([
       request(magazines.put({ id: 1, name: 'Magazine 1' })),
@@ -93,18 +93,18 @@ describe('idb-request', function() {
     ])
   })
 
-  it('request(cursor, iterator)', () => {
-    const wBooks = db.transaction(['books'], 'readwrite').objectStore('books')
+  it('request(cursor, iterator)', function() {
+    var wBooks = db.transaction(['books'], 'readwrite').objectStore('books')
     return Promise.all([
       request(wBooks.put({ title: 'Quarry Memories', author: 'Fred', isbn: 123456 })),
       request(wBooks.put({ title: 'Water Buffaloes', author: 'Fred', isbn: 234567 })),
       request(wBooks.put({ title: 'Bedrock Nights', author: 'Barney', isbn: 345678 })),
-    ]).then(() => {
-      const rBooks = db.transaction(['books'], 'readonly').objectStore('books')
-      const req = rBooks.openCursor()
-      const result = []
+    ]).then(function() {
+      var rBooks = db.transaction(['books'], 'readonly').objectStore('books')
+      var req = rBooks.openCursor()
+      var result = []
 
-      return request(req, iterator).then(() => {
+      return request(req, iterator).then(function() {
         expect(result).length(3)
       })
 
@@ -118,9 +118,9 @@ describe('idb-request', function() {
   it('handle errors', function() {
     return request(idb.open(dbName, 2)).catch(function(err) {
       expect(err.name).equal('VersionError')
-      const wBooks1 = db.transaction(['books'], 'readwrite').objectStore('books')
+      var wBooks1 = db.transaction(['books'], 'readwrite').objectStore('books')
       return request(wBooks1.add({ isbn: 1 })).then(function() {
-        const wBooks2 = db.transaction(['books'], 'readwrite').objectStore('books')
+        var wBooks2 = db.transaction(['books'], 'readwrite').objectStore('books')
         return request(wBooks2.add({ isbn: 1 })).catch(function(err) {
           expect(err.name).equal('ConstraintError')
         })
